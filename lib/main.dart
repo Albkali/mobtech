4c01bfba8c0cc6ad183c8d9fe1fb3a5edec40306
Yasser Albkali <Yasser.Albkali@hotmail.com>
2021-07-02 19:55:23 +0300

    IU-Menu-Carousel

--- lib/main.dart
+++ lib/main.dart
@@ -1,113 +1,131 @@
 import 'package:flutter/material.dart';
+// import 'package:carousel_pro/carousel_pro.dart';
+import 'package:carousel_pro_nullsafety/carousel_pro_nullsafety.dart';
 
-void main() {
-  runApp(MyApp());
-}
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
+      debugShowCheckedModeBanner: false,
+      title: " Mobtech",
+      home: Directionality(
+        textDirection: TextDirection.rtl,
+        child: Scaffold(
+          appBar: AppBar(
+            title: Text("Mobtech"),
+            backgroundColor: Colors.blue,
+            actions: [IconButton(onPressed: () {}, icon: Icon(Icons.search))],
+            centerTitle: true,
+            elevation: 30,
+            // leading: IconButton(onPressed: () {},icon: Icon(Icons.search)),
+            brightness: Brightness.dark,
+          ),
+          drawer: Drawer(
+            child: ListView(
+              children: [
+                UserAccountsDrawerHeader(
+                  accountName: Text("Yasser Albkali"),
+                  accountEmail: Text("Yasser.Albkali@gmail.com"),
+                  currentAccountPicture:
+                      CircleAvatar(child: Icon(Icons.person)),
+                  decoration: BoxDecoration(
+                    color: Colors.red,
+                    image: DecorationImage(
+                      image: NetworkImage(
+                          "https://miro.medium.com/max/1200/1*zH86wd-3URNeM_MoeaBQuQ.jpeg"),
+                      fit: BoxFit.fill,
+                    ),
+                  ),
+                ),
+                ListTile(
+                  title: Text(
+                    "الصفحة الرئيسية",
+                    style: TextStyle(color: Colors.black, fontSize: 18),
+                  ),
+                  leading: Icon(Icons.home, color: Colors.blue, size: 25),
+                  onTap: () {},
+                  // trailing: Icon(Icons.hot_tub),
+                  // subtitle: Text("yaya"),
+                  // isThreeLine: true,
+                  // dense: true,
+                  // contentPadding: EdgeInsets.all(10),
+                ),
+                ListTile(
+                  title: Text(
+                    "الاقسام",
+                    style: TextStyle(color: Colors.black, fontSize: 18),
+                  ),
+                  leading: Icon(Icons.category, color: Colors.blue, size: 25),
+                  onTap: () {},
+                ),
+                Divider(color: Colors.blue, height: 10, thickness: 0.1),
+                ListTile(
+                  title: Text(
+                    "حول الجوال",
+                    style: TextStyle(color: Colors.black, fontSize: 18),
+                  ),
+                  leading: Icon(Icons.info, color: Colors.blue, size: 25),
+                  onTap: () {},
+                ),
+                ListTile(
+                  title: Text(
+                    "الاعدادات",
+                    style: TextStyle(color: Colors.black, fontSize: 18),
+                  ),
+                  leading: Icon(Icons.settings, color: Colors.blue, size: 25),
+                  onTap: () {},
+                ),
+                ListTile(
+                  title: Text(
+                    "تسجيل الخروج",
+                    style: TextStyle(color: Colors.black, fontSize: 18),
+                  ),
+                  leading:
+                      Icon(Icons.login_outlined, color: Colors.blue, size: 25),
+                  onTap: () {},
+                ),
+              ],
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
+          ),
+          body: Column(
+            children: <Widget>[
+              SizedBox(
+                height: 250.0,
+                width: double.infinity,
+                child: Carousel(
+                  boxFit: BoxFit.cover,
+                  autoplay: true,
+                  animationCurve: Curves.fastOutSlowIn,
+                  animationDuration: Duration(milliseconds: 1000),
+                  dotSize: 8.0,
+                  dotIncreasedColor: Color(0xFFFF335C),
+                  dotBgColor: Colors.transparent,
+                  dotPosition: DotPosition.bottomCenter,
+                  dotVerticalPadding: 10.0,
+                  showIndicator: true,
+                  indicatorBgPadding: 7.0,
+                  images: [
+                    Image.asset(
+                      'assets/images/slider_1.jpeg',
+                      fit: BoxFit.fill,
+                    ),
+                    Image.asset(
+                      'assets/images/slider_2.png',
+                      fit: BoxFit.fill,
+                    ),
+                    Image.asset(
+                      'assets/images/slider_3.jpeg',
+                      fit: BoxFit.fill,
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
